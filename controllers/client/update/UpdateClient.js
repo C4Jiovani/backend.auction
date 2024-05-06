0e6bfba363ddeb9de8996ce8b0a09647e04f3c6c
C4Jiovani <zmcracky@gmail.com>
2024-05-06 23:53:41 +0300

    socket Mis en place et fonctionnel

--- controllers/client/update/UpdateClient.js
+++ controllers/client/update/UpdateClient.js
@@ -22,6 +22,7 @@
 
 import { db } from "../../../config/bd.js";
 
+
 export const updateUser = (req, res) => {
   const UserId = req.params.IdUser;
   const q =
@@ -33,3 +34,24 @@ export const updateUser = (req, res) => {
     return res.status(200).json("User has been updated");
   });
 };
+///avec image pdp
+// import { db } from "../../../config/bd.js";
+// import { upload } from "../../../config/multer.js";
+
+// export const updateUser = (req, res) => {
+//   upload.single('image', (req, res, (err) => {
+//     if (err) {
+//       return res.status(400).json({ message: 'Failed to upload profile picture' })
+//     }
+//     const UserId = req.params.IdUser;
+//     const imagePath = req.body.path
+//     const q =
+//       "UPDATE user SET `name`=?, `userName`=?, `email`=?,`profile`=?, `numTelephone`=? WHERE `IdUser`=?";
+//     const values = [req.body.name, req.body.userName, req.body.email,imagePath, req.body.numTelephone];
+
+//     db.query(q, [...values, UserId], (err, data) => {
+//       if (err) return res.status(500).json("Error updating user");
+//       return res.status(200).json("User has been updated");
+//     });
+//   }))
+// };
--- index.js
+++ index.js
@@ -1,54 +1,130 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import AuthRouteAdminRoute from "./routes/admin/AuthRouteAdmin.js";
+// import AuthRouteUser from "./routes/client/AuthRouteUser.js";
+// import cookieParser from "cookie-parser";
+// import { db } from "./config/bd.js";
+// // import { Server } from "socket.io";
+// import http, { Server } from "http";
+// import multer from 'multer';
+// import path from "path";
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import { Server as SocketIOServer } from 'socket.io'
+
+
+// // const socketIo = require()
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const upload = multer({ dest: 'uploads/' });
+
+// const app = express();
+// dotenv.config();
+
+// // Middleware pour l'analyse des données JSON dans les requêtes
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use(express.json());
+// app.use(cookieParser());
+// // Middleware pour gérer les problèmes de CORS (Cross-Origin Resource Sharing)
+// app.use(cors());
+
+// const server = http.createServer(app); // Création du serveur HTTP
+// const io = new SocketIOServer(server)
+
+// io.on('connection', (socket) => {
+//   console.log('Nouvelle Connection', socket.id)
+
+//   socket.on('newComment', (comment) => {
+//     const insterQuery = "INSERT INTO comment (IdUser,montant) VALUES (?,?)"
+//     db.query(insterQuery, [comment.user, comment.montant], (err, res) => {
+//       if (err) {
+//         console.log('erreur de l insertion du commentaire', err)
+//       }
+//       else {
+//         console.log('commentaire insérer à la base de donnée')
+//         io.emit('newComment', comment)
+//       }
+//     })
+//   })
+//   socket.on('disconnect',()=>{
+//     console.log('Client deconnecté :',socket.id)
+//   })
+// })
+
+
+// app.use("/admin", AuthRouteAdminRoute);
+// app.use("/user", AuthRouteUser);
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to our E-commerce APIs");
+// });
+
+// const port = 5000;
+
+// // Démarrer le serveur HTTP et afficher un message lorsque le serveur est prêt
+// server.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import AuthRouteAdminRoute from "./routes/admin/AuthRouteAdmin.js";
 import AuthRouteUser from "./routes/client/AuthRouteUser.js";
 import cookieParser from "cookie-parser";
-// import { Server } from "socket.io";
+import { db } from "./config/bd.js";
 import http from "http";
+import { Server as SocketIOServer } from "socket.io";
 import multer from 'multer';
 import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// const path = require('path');
+
 const upload = multer({ dest: 'uploads/' });
+
 
 const app = express();
 dotenv.config();
-
-// Middleware pour l'analyse des données JSON dans les requêtes
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use(express.json());
 app.use(cookieParser());
-// Middleware pour gérer les problèmes de CORS (Cross-Origin Resource Sharing)
-app.use(cors());
+app.use(cors()); // Middleware pour gérer les problèmes de CORS
 
-const server = http.createServer(app); // Création du serveur HTTP
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true // autorise les cookies et les informations d'authentification
+  }
+});
 
-// const io = new Server(server); // Initialisation de Socket.IO
-// const io = new Server({
-//   cors: {
-//     origin: "http://localhost:8080"
-//   }
-// }); // Initialisation de Socket.IO
+io.on('connection', (socket) => {
+  console.log('Nouvelle Connection', socket.id);
 
-// io.on("connect", (socket) => {
-//   // console.log("A client connected to WebSocket server");
-//   socket.on('comment', (data) => {
-//     // Diffuser le commentaire à tous les utilisateurs connectés
-//     io.emit('newComment', data);
-//   });
+  socket.on('newComment', (comment) => {
+    const insterQuery = "INSERT INTO comment (IdUser,montant,name) VALUES (?,?,?)";
+    db.query(insterQuery, [comment.user, comment.montant,comment.nameL], (err, res) => {
+      if (err) {
+        console.log('erreur de l insertion du commentaire', err);
+      } else {
+        console.log('commentaire insérer à la base de donnée');
+        io.emit('newComment', comment);
+      }
+    });
+  });
 
-//   // Gérer les événements WebSocket ici
-//   socket.on("disconnect", () => {
-//     console.log("A client disconnected from WebSocket server");
-//   });
-// });
-
-
+  socket.on('disconnect', () => {
+    console.log('Client deconnecté :', socket.id);
+  });
+});
 
 app.use("/admin", AuthRouteAdminRoute);
 app.use("/user", AuthRouteUser);
@@ -59,7 +135,6 @@ app.get("/", (req, res) => {
 
 const port = 5000;
 
-// Démarrer le serveur HTTP et afficher un message lorsque le serveur est prêt
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
